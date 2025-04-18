a2101a680abdb405726411f95c0772f1223bfc78
iRyann <ryanbouchou@orange.fr>
2025-04-18 13:18:09 +0200

    Bar generator

--- jeu.py
+++ jeu.py
@@ -1,10 +1,13 @@
 from presentation import *
 from mario import *
+from timerBarres import *
 
 class Jeu:
     def __init__(self):
         self.presentation = Presentation() 
         self.mario = Mario() 
+        self.timerBarres = TimerBarres()
+        self.bars = []
 
     # ----------------------------------------------------------------------------
     # Méthode contenant la boucle principale du jeu
@@ -15,8 +18,9 @@ class Jeu:
 	
             self.mario.actualiser(self.presentation.lireEvenement())
 
+            if self.timerBarres.actualiser(self.presentation.lireEvenement()):
+                pass
             # Mettre à jour l'image à l'écran
-
             self.actualiserEcran()
 
             # attendre 100 millisecondes (délai de référence)
---
+++ timerBarres.py
@@ -0,0 +1,20 @@
+from random import randint
+
+class TimerBarres:
+    def __init__(self):
+        self.delai = 85
+        self.new_time = 500
+        self.current_delai = 85
+        
+    def actualiser(self, evenement):
+        self.delai -= 1
+        self.new_time -= 1
+
+        if self.delai <= 0:
+            if(self.new_time <= 0):
+                self.new_time = 500
+                self.current_delai = randint(70,100)
+            
+            self.delai = self.current_delai
+            
+        return False if self.delai > 0 else True
